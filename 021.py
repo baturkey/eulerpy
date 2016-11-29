26c969c0d57ed4b2dcd7e0452030cfadf5d7d4d2
Jeffrey Liu <jeffrey@ipro.us>
2016-11-29 16:27:23 -0500

    Remove unneeded import statements

--- 021.py
+++ 021.py
@@ -13,8 +13,6 @@ For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22,
 
 Evaluate the sum of all the amicable numbers under 10000.
 """
-from math import sqrt
-from itertools import combinations
 
 def d(n):
     output = 1
